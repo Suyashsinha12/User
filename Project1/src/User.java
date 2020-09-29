e50c6f619889c22c0bed1385568ffe61838cfb92
suyashkumari <suyashsinha12@gmail.com>
2020-09-29 17:20:29 +0530

    Adding Project1 here...

---
+++ Project1/src/User.java
@@ -0,0 +1,13 @@
+package Project1; 
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Date d1 = new Date();
+		d1.setDate(7, 9, 2020);
+	
+		d1.displayDate();
+	}
+
+}
